8eb2f14a953d2e20bb4c88f74f3addadabd23042
JuanTamarit <116755072+JuanTamarit@users.noreply.github.com>
2023-03-10 13:24:25 +0100

    Corrección visibilidad nav
    Corrección del problema:
    En desktop no se veía el texto del menú de navegación

--- js/main.js
+++ js/main.js
@@ -4,8 +4,12 @@ const menuDesplegable = document.querySelector('.menu-desplegable');
 
 const imagen = document.querySelector('.section__hero__imagen');
 
+const topbar = document.querySelector('.topbar');
+
 iconoMenuHamburguesa.addEventListener('click', function(){
     menuDesplegable.classList.toggle('mostrarmenu');
     imagen.classList.toggle('ocultar-imagen');
+    topbar.classList.toggle('ocultar-margin-topbar');
+
 
 });
