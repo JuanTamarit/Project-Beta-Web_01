2197b9f5b1062e382d997aff0646206cdfb8e968
JuanTamarit <116755072+JuanTamarit@users.noreply.github.com>
2023-03-10 14:56:35 +0100

    Ajustes tipográficos de la home
    Creo una función de js para retocar el aspecto de los párrafos en mobile

--- js/main.js
+++ js/main.js
@@ -10,6 +10,25 @@ iconoMenuHamburguesa.addEventListener('click', function(){
     menuDesplegable.classList.toggle('mostrarmenu');
     imagen.classList.toggle('ocultar-imagen');
     topbar.classList.toggle('ocultar-margin-topbar');
-
-
 });
+
+
+function retocarTipografia() {
+    var anchoViewport = window.innerWidth;
+    var tituloSectionColabora = document.querySelector(".section__colabora__titulos"); 
+    var tituloSectionIdeas = document.querySelector(".section__ideas__titulos")
+    if (anchoViewport < 1194) {
+      tituloSectionColabora.innerHTML = "<h2>Colabora con <br>el proyecto</h2><h5>Prendas exclusivas <br>diseñadas por artistas</h5>";
+      tituloSectionIdeas.innerHTML ="<h2>Ideas para <br>el cambio</h2>";
+
+
+    } else {
+      tituloSectionColabora.innerHTML = "<h2>Colabora con el proyecto</h2><h5>Prendas exclusivas diseñadas por artistas</h5>"; 
+      tituloSectionIdeas.innerHTML ="<h2>Ideas para el cambio</h2>";
+
+    }
+  }
+  
+  retocarTipografia();
+  
+  window.addEventListener("resize", retocarTipografia);
